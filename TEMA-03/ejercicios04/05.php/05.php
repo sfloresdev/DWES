b4af144ddcb6acbfe3e33d09883a7373db3773a2
Sergio Flores <sergiofloresperaltarafael@gmail.com>
2025-10-09 14:14:26 +0200

    Terminar php05

---
+++ TEMA-03/ejercicios04/05.php/05.php
@@ -0,0 +1,23 @@
+<?php
+
+$nombre = $_POST['nombre'] ?? 'Anonimo';
+$apellidos = $_POST['apellidos'] ?? '';
+$sexo = $_POST[]
+
+
+$msg[] = "Bienvenido $nombre";
+
+if (isset($_POST['edad']))
+    $edad = $_POST['edad'];
+
+if (isset($_POST['hobbies']))
+{
+    $hobbiesSeleccionados = $_POST['hobbies']; 
+    foreach($hobbiesSeleccionados as $h)
+    {
+        
+    }
+}
+else
+    $hobbies = "no tiene aficiones";
+?>
